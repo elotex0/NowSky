85ff80ed78e6f54cc6ec277c461290d50b52c101
elotex0 <justin.kettner14@icloud.com>
2026-01-09 22:09:32 +0100

    Update weather data URL in bw.js

--- api/wetterlage/bw.js
+++ api/wetterlage/bw.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   try {
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    const url = "https://www.dwd.de/DWD/wetter/wv_allg/deutschland/text/vhdl13_dwoh.html";
+    const url = "https://www.dwd.de/DWD/wetter/wv_allg/deutschland/text/vhdl13_dwsg.html";
 
     // Fetch als ArrayBuffer, korrekt decodieren
     const response = await fetch(url);
