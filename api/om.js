c01bbe1c18dc3a714a8e8dd7baa754e0b12355b1
elotex0 <justin.kettner14@icloud.com>
2026-03-15 15:48:57 +0100

    Update om.js

--- api/om.js
+++ api/om.js
@@ -5,8 +5,10 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Cache-Control", "public, max-age=0, s-maxage=3600, stale-while-revalidate=0");
 
   if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Max-Age", "86400");
     return res.status(204).end();
   }
 
