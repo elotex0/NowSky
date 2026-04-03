cb1fb947ca93116cef9485d0cecc556011b437ae
elotex0 <justin.kettner14@icloud.com>
2026-04-03 14:33:44 +0200

    Update blitze.js

--- api/blitze.js
+++ api/blitze.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
   const latNum   = parseFloat(lat);
   const lonNum   = parseFloat(lon);
-  const radiusKm = parseFloat(radius ?? 5);
+  const radiusKm = parseFloat(radius ?? 10);
 
   const fmtDE = (isoStr) =>
     new Date(isoStr).toLocaleString("de-DE", {
