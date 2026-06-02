736171f76180963e6815ef21a9e108737f2b157d
elotex0 <justin.kettner14@icloud.com>
2026-06-03 01:05:52 +0200

    Update stormtracking.js

--- api/stormtracking.js
+++ api/stormtracking.js
@@ -456,7 +456,7 @@ export default async function handler(req, res) {
     const results = await Promise.allSettled(
       [5, 10, 15, 20].map(async (offset) => {
         const filename = buildFilename(offset);
-        const url = `https://opendata.dwd.de/weather/radar/konrad3d/KONRAD3D_20260602T162000.xml`;
+        const url = `https://opendata.dwd.de/weather/radar/konrad3d/KONRAD3D_20260602T162500.xml`;
         const r = await fetch(url, {
           headers: { "User-Agent": "konrad3d-api/1.0", "Connection": "keep-alive" },
           signal: AbortSignal.timeout(8000),
