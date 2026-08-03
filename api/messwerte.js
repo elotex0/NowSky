633e47a760cc636b7ab5fdeca39eaa7349b02c6c
elotex0 <justin.kettner14@icloud.com>
2026-08-03 21:12:59 +0200

    Update messwerte.js

--- api/messwerte.js
+++ api/messwerte.js
@@ -27,7 +27,7 @@ function inGermanyBbox(station) {
 }
 
 // Erlaubte Quellen.
-const ALLOWED_SOURCES = ['dwd', 'iem'];
+const ALLOWED_SOURCES = ['dwd', 'iem', 'meteofrance'];
 
 function isAllowedSource(station) {
   return ALLOWED_SOURCES.includes(station.source);
